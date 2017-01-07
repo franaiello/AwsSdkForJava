5a25f2fb835bb46fb14984baa7c406446302c8c0
Francis Aiello <fran.aiello@dev9.com>
2017-01-07 12:16:22 -0800

    Adding Javadoc to reference AWS documentation

--- src/main/java/com/aiello/exercise/sns/MessageSender.java
+++ src/main/java/com/aiello/exercise/sns/MessageSender.java
@@ -6,11 +6,15 @@ import com.amazonaws.services.sns.model.*;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * The following Java class was taken from AWS
+ * @see <a href="http://docs.aws.amazon.com/sns/latest/dg/sms_publish-to-phone.html">Sending an SMS Message</a>
+ */
 public class MessageSender {
     public static void main(String[] args) {
         AmazonSNSClient snsClient = new AmazonSNSClient();
         String message = "My SMS message via AWS SDK";
-        String phoneNumber = "<phone number>";
+        String phoneNumber = "+1XXX5550100";  // TODO: Replace with your own phone number to give it a try
         Map<String, MessageAttributeValue> smsAttributes = new HashMap<String, MessageAttributeValue>();
         sendSMSMessage(snsClient, message, phoneNumber, smsAttributes);
     }
